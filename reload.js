4b3c0b7288b3c308ca71b39c5574025acf120007
Good Boy Ninja <66829812+GoodBoyNinja@users.noreply.github.com>
2022-07-09 20:37:39 +0200

    Update reload.js
    + reloadJS now deletes the previous copy of the folder to make sure changes are applied correctly.
    + code quality improvements.

--- reload.js
+++ reload.js
@@ -1,30 +1,58 @@
-// NOTE: run in nodemon to watch for changes
+// Created by GoodBoy.Ninja
+// run using nodemon to watch for changes in src folder and copy to adobe extensions folder
+// more info: https://github.com/GoodBoyNinja/CEP-ReloadJS
+
+// prepare
 const gulp = require('gulp');
 const os = require("os");
 const path = require("path");
 const fs = require("fs");
 const isWin = process.platform === "win32";
-
-// user properties
-const copyMe = 'src'; // sub folder
-const thisFolderName = path.basename(__dirname);
-const targetFolderName = thisFolderName + " Dev Reloading Copy";
-
 var userName = os.userInfo().username;
 var extensionsFolder = isWin ? `C:/Users/${userName}/AppData/Roaming/Adobe/CEP/extensions` : `/Users/${userName}/Library/Application Support/Adobe/CEP/extensions`;
+
+
+// user properties
+const pathToSrc = 'src'; // (RELATIVE!!!)
+const projectName = path.basename(__dirname); // get the name of the project folder
+const targetFolderName = projectName + "_DevCopy";
+
+// solve paths
+const absPathToSrc = path.join(__dirname, pathToSrc);
+const srcFolderName = path.basename(absPathToSrc);
+const targetFolder = path.join(extensionsFolder, targetFolderName);
+
+
+
+// this function deletes any previous copies of the src folder in the adobe extensions folder
+function deletePrevious() {
+    // delete previous copies in the adobe extensions folder, synchronously
+    const exists = fs.existsSync(targetFolder);
+    if (exists) {
+        console.log("deleting:", targetFolder + "\n\n");
+        fs.rmSync(targetFolder, { recursive: true });
+    }
+}
+
+// this function copies the src folder to the adobe extensions folder
 function copySrc() {
-    fs.access(path.join(__dirname, copyMe), fs.constants.F_OK, (err) => {
+
+
+    fs.access(absPathToSrc, fs.constants.F_OK, (err) => {
         if (err) {
             console.log('directory to copy does not exist');
             return;
         }
     });
     // include hidden files
-    gulp.src(path.join(__dirname, copyMe + '/**/*'), { dot: true })
-        .pipe(gulp.dest(path.join(extensionsFolder, targetFolderName)));
+    gulp.src(path.join(absPathToSrc + '/**/*'), { dot: true })
+        .pipe(gulp.dest(targetFolder));
+
+    console.log(`"${srcFolderName}" from "${projectName}" copied to ${targetFolder}`);
 
 };
 
 
-// copy
+// run
+deletePrevious();
 copySrc();
